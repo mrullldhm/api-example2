1c1df85b785de6419452aab6c2b9788f88cd6ece
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-01 05:33:32 +0800

    Update server.js & dependency

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRouter from "./routes/auth.Route.js";
+import todoRouter from "./routes/todo.route.js";
+import dotenv from "dotenv";
+import authMiddleware from "./middleware/auth.middleware.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8383;
+const PORT = process.env.PORT || 5000;
 
 const __filename = fileURLToPath(import.meta.url); // Get the File Path
 const __dirname = dirname(__filename); // Get the Directory Path
@@ -13,11 +19,14 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public"))); // Serving Static Files
 // Middleware (custom)
 
-
 // app.method (route, handler)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+// Routes
+app.use("/auth", authRouter);
+app.use("/todo", authMiddleware, todoRouter);
 
 // app.listen(port, callback)
 app.listen(PORT, () => {
